cd03d738dfa9b22a8870b876c2ed7aeaffa19a74
Dhurjati-Sharma <85036440+Dhurjati-Sharma@users.noreply.github.com>
2021-08-25 12:58:43 -0700

    Create rrr.js

---
+++ rrr.js
@@ -0,0 +1,47 @@
+var d = document
+var logincode;
+var myxyzyhyghyy
+if (window.location.search == "?chkkey") {
+  if (EnterYourkey == key) {
+
+  }
+  else {
+    location.href = 'key-error.html'
+  }
+}
+if (window.location.search == "?open") {
+
+
+  $(document).ready(function () {
+    $("#exampleModal").modal('show');
+
+    window.onload = function hev() {
+      if (d.getElementById("g").style.display = "block") { d.getElementById("g").style.display = "none" }
+      if(requirepassword==true){
+    d.getElementById("passname").innerHTML = logincode
+    d.getElementById("passname").title = "Password: " + logincode}
+    else{
+    d.getElementById("dhujtjtfhyf").style.display = "none"
+    }    $("#f").addClass("hide");
+      return true
+
+    }
+    document.getElementById("qatempts").innerHTML = localStorage.getItem("pointswas") 
+    document.getElementById("qatempts").title = "Incorrect Attempts: " +  localStorage.getItem("pointswas")
+    document.getElementById("hepmemememememem").innerHTML = localStorage.getItem("pointscas") 
+    document.getElementById("hepmemememememem").title = "Correct Attempts: " +  localStorage.getItem("pointscas")
+    
+    d.getElementById("analyticname").innerHTML = localStorage.getItem("pointsname")
+    d.getElementById("analyticname").title = "Username: " + localStorage.getItem("pointsname")
+    d.getElementById("dhujtjtfhyf").style.display = "block"
+    document.getElementById("myhellclose").style.display = "none"
+    document.getElementById("myhellclosed").style.display = "block"
+    document.getElementById("myidop").style.display = "none"
+    document.getElementById("myidopp").style.display = "block"
+    d.getElementById("chair1").style.display = "none"
+    d.getElementById("chair2").style.display = "none"
+    d.getElementById("chair3").style.display = "none"
+
+    document.title = "Analytics/Information"
+  });
+}
